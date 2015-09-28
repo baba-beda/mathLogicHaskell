b335ba9858924ebf97fc62092b464d8a17e22082
baba-beda <darya.nightmare@gmail.com>
2015-09-28 17:10:32 +0300

    first seems working, but i need way to parse back the expression

--- Task1.hs
+++ Task1.hs
@@ -6,28 +6,30 @@ import Checking
 
 main = do
     f <- readFile "task1.in"
-    let proof = lines f
-    putStrLn $ show (head proof)
+    let proof = map read (lines f) :: [Expr]
+    let annotated = check proof 0 proof
+    checkAnnotationAndPrint (head annotated) (tail annotated) 0
     
 
 
 check :: [Expr] -> Int -> [Expr] -> [AnnotatedExpr]
+check [] _ _ = []
 check (e:restProof) i proof 
     | isAxiom e > 0 = Axiom (isAxiom e) e : check restProof (i + 1) proof
     | otherwise = let (a, b) = findAB e 0 (head proof) (tail (take i proof)) (take i proof) in MP a b e : check restProof (i + 1) proof
-check [] _ _ = []
 
 findAB :: Expr -> Int -> Expr ->  [Expr] -> [Expr] ->  (Int, Int)
+
 findAB e i (Impl a b) rest proof
-    | e == b = let k = (findA a 0 proof) in (if k > 0 then (i, k) else (findAB e (i+1) (head rest) (tail rest) proof))
+    | length rest == 0 = (findA a 0 proof, i)
+    | e == b = let k = (findA a 0 proof) in (if k >= 0 then (k, i) else (findAB e (i+1) (head rest) (tail rest) proof))
     | otherwise = findAB e (i + 1) (head rest) (tail rest) proof
-findAB _ _ _ [] _ = (-1, -1)
 
 findA :: Expr -> Int -> [Expr] -> Int
+findA _ _ [] = -1
 findA a i (e:proof)
     | a == e = i
     | otherwise = findA a (i + 1) proof
-findA _ _ [] = -1
     
 
 data AnnotatedExpr =
@@ -35,3 +37,13 @@ data AnnotatedExpr =
     | MP Int Int Expr 
     | NotAnnotated Expr
     deriving (Show)
+
+checkAnnotationAndPrint (Axiom i e) annotated j = do
+    putStrLn $ "(" ++ (show (j + 1)) ++ ") " ++ (show e) ++ " axiom " ++ (show i)
+    if (length annotated == 0) then putStrLn "Proof is correct"  else checkAnnotationAndPrint (head annotated) (tail annotated) (j + 1)
+
+
+checkAnnotationAndPrint (MP a b e) annotated j = do
+    let msg = if (a == -1) then "(" ++ (show (j + 1)) ++ ") " ++ (show e) ++ "\n" ++ "Proof is incorrect from statement " ++ (show (j + 1)) else "(" ++ (show (j + 1)) ++ ") " ++ (show e) ++ " MP " ++ (show (a + 1)) ++ ", " ++ (show (b + 1))
+    putStrLn msg
+    if (a /= -1 && (length annotated) == 0) then putStrLn "Proof is correct" else (if (a == -1 || (length annotated) == 0) then return () else checkAnnotationAndPrint (head annotated) (tail annotated) (j + 1))
