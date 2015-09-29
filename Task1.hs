2bd861a0a006b67224fed3029a69918a1c2b3cea
baba-beda <darya.nightmare@gmail.com>
2015-09-30 01:21:19 +0300

    trying to remove errors

--- Task1.hs
+++ Task1.hs
@@ -3,6 +3,10 @@ module Main where
 import Grammar
 import System.IO
 import Checking
+import Control.Monad
+import Control.Monad.ST
+import Data.List
+import Data.Array.Unboxed
 import Data.Array.ST
 import Data.STRef
 import qualified Data.Map as Map
@@ -11,11 +15,13 @@ import System.Exit (exitSuccess)
 main = do
     f <- readFile "task1.in"
     let proof = map read (lines f) :: [Expr]
-    checkProof proof
+    res <- checkProof proof
+    mapM_ putStrLn res
+    
 
-checkProof proof = runSTArray $ do
+checkProof proof  = runSTArray $ do
     let n = (length proof)
-    proofArray <- newArray (0, n - 1) proof :: ST s (STArray s Int Expr)
+    proofArray <- newListArray (0, n - 1) proof :: ST s (STArray s Int Expr)
     mapProof <- newSTRef Map.empty
     neededA <- newSTRef Map.empty
     resultsMP <- newSTRef Map.empty 
@@ -24,31 +30,34 @@ checkProof proof = runSTArray $ do
         nA <- readSTRef neededA
         resMP <- readSTRef resultsMP
         curExp <- readArray proofArray i
-        putStr $ "(" ++ (show (i + 1)) ++ ") " ++ (show curExp) ++ " "
         let a = isAxiom curExp
 
         if (a > 0) then do
-            putStr $ "axiom " ++ (show a)
-        else do
-            if (Map.member curExp resMP) then do
-                (f, s) <- lookup curExp resMP
-                putStr $ "MP " ++ (show (f + 1)) ++ ", " ++ (show (s + 1))
-            else do
-                putStr $ "The proof is incorrect from statement " ++ (show (i + 1))
-                exitSuccess
-
-        case curExp of (Impl a b) -> do
-            if (Map.member a mPr) then do
-                num <- Map.lookup a mPr
-                writeSTRef resultsMP (Map.insert b (num, i))
-            else writeSTRef neededA (Map.insert nA a i)
-        if (Map.member curExp neededA) then do
-            index <- Map.lookup curExp neededA
-            Impl a b <- readArray proofArray index
-            writeSTRef resultsMP (Map.insert b (i, index))
-            writeSTRef neededA (Map.delete nA curExp)
-        else
+            let msg = "(" ++ (show (i + 1)) ++ ") " ++ (show curExp) ++ " axiom " ++ (show a)
+            putStrLn msg
             return ()
-        writeSTRef mapProof (Map.insert curExp i)
+        else do
+            case Map.lookup curExp resMP of
+                Nothing -> do
+                    let msg = "(" ++ (show (i + 1)) ++ ") " ++ (show curExp) ++ "\n The proof is incorrect from statement " ++ (show (i + 1))
+                    putStrLn msg
+                    return ()
+                Just (f, s) -> do
+                    let msg = "(" ++ (show (i + 1)) ++ ") " ++ (show curExp) ++ " MP " ++ (show (f + 1)) ++ ", " ++ (show (s + 1))
+                    putStrLn msg
+                    return () 
 
+        case curExp of 
+            (Impl a b) -> do
+                    case Map.lookup a mPr of
+                        Nothing -> writeSTRef neededA (Map.insert a i nA)
+                        Just num -> writeSTRef resultsMP (Map.insert b (num, i) resMP)
+        case Map.lookup curExp nA of
+            Nothing -> return ()
+            Just index -> do
+                Impl a b <- readArray proofArray index
+                writeSTRef resultsMP (Map.insert b (i, index) resMP)
+                writeSTRef neededA (Map.delete curExp nA)
+        writeSTRef mapProof (Map.insert curExp i mPr)
+        
 
